0de14d594f8caa8b7acc489a12e1ea041f308912
gonzalofh <gonzalofh@users.noreply.github.com>
2021-01-31 19:02:13 -0500

    refactored Plaid API client to external package

--- fetch-data/index.js
+++ fetch-data/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
 import fs from 'fs';
-import plaidClient from './plaidClient.js';
+import { getAccount } from 'plaid-client';
 
 const rawDataBuildDirectory = process.env['RAW_DATA_BUILD_DIR'];
 
 export default function(institution) {
     return (done) => {
-        return plaidClient.getAccounts(institution['accessToken'])
+        return getAccount(institution['accessToken'], 'arraybuffer')
             .then(res => {
                 fs.writeFile(`${rawDataBuildDirectory}/${institution['name']}.json`, res['data'], done);
             })
